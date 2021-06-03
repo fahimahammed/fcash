8e23e64879b19d6da210b629feff19d92f472a0a
fahimahammed <fahimahammed.cse@gmail.com>
2021-06-03 23:24:43 +0600

    native app

---
+++ constants/images.js
@@ -0,0 +1,29 @@
+const wallieLogo = require("../assets/images/wallie-logo.png");
+const logo = require("../assets/images/logo.png");
+const banner = require("../assets/images/banner.png");
+const cashback = require("../assets/images/cashback.png");
+const recharge = require("../assets/images/recharge.png");
+const refer = require("../assets/images/refer.png");
+const discount = require("../assets/images/discount.png");
+const homeLogo = require("../assets/images/home-logo.png");
+const promoBanner = require("../assets/images/promo-banner.png");
+const focus = require("../assets/images/focus.png");
+
+// Dummy
+const usFlag = require("../assets/images/us-flag.jpg");
+
+
+export default {
+    wallieLogo,
+    banner,
+    promoBanner,
+    focus,
+    logo,
+    cashback,
+    recharge,
+    refer,
+    discount,
+    homeLogo,
+    // Dummy
+    usFlag
+}
